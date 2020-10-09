231c4615ebf882ab5c90107783e91276669ed0d5
fenghaojiang <457420285@qq.com>
2020-10-09 23:22:53 +0800

    Create pat1038.cpp

---
+++ pat1038.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool cmp1(string a, string b) {
+	return a+b < b+a;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<string> v(n);
+	for(int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end(), cmp1);
+	string final = "";
+	for(int i = 0; i < n; i++) {
+		final += v[i];
+	} 
+	while(final.length() != 0 && final[0] == '0') {
+		final.erase(final.begin());
+	}
+	if(final.length() == 0) cout << 0;
+	cout << final << endl;
+	return 0;
+}
